b115e5c1ddf4b0a318dee3dc5c9f88bd37f006fa
John Hutchinson <johnhutch@gmail.com>
2014-09-04 01:53:25 -0400

    fixed masthead button underline, removed twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
     # Include default devise modules. Others available are:
     # :token_authenticatable, :lockable, :timeoutable and :activatable
-    devise :database_authenticatable, :registerable, :omniauthable,
+    devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
 
     # Setup accessible (or protected) attributes for your model
